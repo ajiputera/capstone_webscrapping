76e4b8de5834a057eb2b9dd6248f568e3dc51ddb
ajiputera <60000102+ajiputera@users.noreply.github.com>
2020-05-18 16:14:16 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# Web-Scrapping using Beautifulsoup
+
+![](plot1.png)
+
+This project is developed as capstone project from Algoritma Academy Data Analystics Specialization. The deliverables are expected from this project is to do simple web-scrapping , from [Monex News](monexnews.com/kurs-valuta-asing.htm?kurs=JPY), in order to get Japan Yen exchange Rate to Indonesian Rupiah for year 2019. 
+
+## Dependencies
+
+- beautifulSoup4
+- pandas
+- flask
+- matplotlib
+
+
+## Rubics
+
+- Environment preparation (2 points)
+- Finding the right key to scrap the data  & Extracting the right information (6 points)
+- Creating data frame & Data wrangling (6 points)
+- Implement it on flask dashboard (2 points)
+
+
--- app.py
+++ app.py
@@ -10,31 +10,55 @@ app = Flask(__name__)
 
 def scrap(url):
     #This is fuction for scrapping
-    url_get = requests.get()
+    url_get = requests.get(url)
     soup = BeautifulSoup(url_get.content,"html.parser")
     
     #Find the key to get the information
-    table = soup.find(___) 
-    tr = table.find_all(___) 
+    #table = soup.find(___) 
+    table = soup.find('table', attrs={'class':'table'})
+    #tr = table.find_all(___) 
+    tr = table.find_all('tr')
 
     temp = [] #initiating a tuple
+    
+    import re
+    listm = ['Januari','Februari','Maret','April','Mei','Juni','Juli','Agustus','September','Oktober','November','Desember']
+    listmn = ['1','2','3','4','5','6','7','8','9','10','11','12']
+
 
     for i in range(1, len(tr)):
         row = table.find_all('tr')[i]
         #use the key to take information here
         #name_of_object = row.find_all(...)[0].text
 
+        #get exchange date
+        ExchangeDate = row.find_all('td')[0].text.replace('\xa0','-')
+        ExchangeDate = ExchangeDate.strip() #for removing the excess whitespace
+        
+        for i in range(len(listm)):  
+            if re.search(listm[i],ExchangeDate):
+               ExchangeDate = re.sub(listm[i],listmn[i],ExchangeDate)
+               break
+        
+        #get exchange ask
+        ask = row.find_all('td')[1].text.replace(',','.')
+        ask = ask.strip() #for removing the excess whitespace
+    
+        #get exchange Bid
+        bid = row.find_all('td')[2].text.replace(',','.')
+        bid = bid.strip() #for removing the excess whitespace
 
-
-
-
-
-        temp.append((___)) #append the needed information 
+        #temp.append((___)) #append the needed information 
+        temp.append((ExchangeDate,ask,bid)) 
     
     temp = temp[::-1] #remove the header
 
-    df = pd.DataFrame(temp, columns = (___)) #creating the dataframe
+    #df = pd.DataFrame(temp, columns = (___)) #creating the dataframe
+    df = pd.DataFrame(temp, columns = ('tanggal','Kurs Jual','Kurs Beli'))
+    
    #data wranggling -  try to change the data type to right data type
+    df['Kurs Beli'] = df['Kurs Beli'].astype('float64')
+    df['Kurs Jual'] = df['Kurs Jual'].astype('float64')
 
    #end of data wranggling
 
@@ -42,10 +66,11 @@ def scrap(url):
 
 @app.route("/")
 def index():
-    df = scrap(___) #insert url here
+    #df = scrap(___) #insert url here
+    df = scrap("https://monexnews.com/kurs-valuta-asing.htm?kurs=JPY&searchdatefrom=01-01-2019&searchdateto=31-12-2019")
 
     #This part for rendering matplotlib
-    fig = plt.figure(figsize=(5,2),dpi=300)
+    #fig = plt.figure(figsize=(5,2),dpi=300)
     df.plot()
     
     #Do not change this part
